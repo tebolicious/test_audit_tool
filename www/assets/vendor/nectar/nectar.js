74be083d64b95e3bbbcf205226a582e660b6ef54
cc2148835 <thendo@bbd.co.za>
2018-01-17 16:18:34 +0200

    First Commit

---
+++ www/assets/vendor/nectar/nectar.js
@@ -0,0 +1,91 @@
+'use strict';
+
+myApp.onPageAfterAnimation('*', function(page) {
+	
+	if(page.name === 'geocomplete') {
+		myApp.params.fastClicksExclude = '^pac-';
+	}
+	else {
+		myApp.params.fastClicksExclude = '';
+	}
+	
+});
+
+myApp.onPageInit('*', function(page) {
+
+/*
+|------------------------------------------------------------------------------
+| Google Analytics
+|------------------------------------------------------------------------------
+*/
+
+ga('send', 'screenview', {
+	screenName: page.name
+});
+
+/*
+|------------------------------------------------------------------------------
+| Chips
+|------------------------------------------------------------------------------
+*/
+
+	/* Delete Chip */
+	$$('.chip-delete').on('click', function(e) {
+    e.preventDefault();
+    var chip = $$(this).parents('.chip');
+		if($$(this).hasClass('confirm')) {
+			myApp.confirm('Do you want to delete this?', function() {
+				chip.remove();
+			});
+		}
+		else {
+			chip.remove();
+		}
+	});
+
+/*
+|------------------------------------------------------------------------------
+| Side Panel
+|------------------------------------------------------------------------------
+*/
+
+$$('.panel-left').on('open', function() {
+	$$('.hamburger').addClass('is-active');
+});
+
+$$('.panel-left').on('close', function() {
+	$$('.hamburger').removeClass('is-active');
+});
+	
+/*
+|------------------------------------------------------------------------------
+| Tooltip
+|------------------------------------------------------------------------------
+*/
+
+	$('[title]').tooltipster({
+		theme: 'tooltipster-borderless'
+	});
+
+	$('[data-tooltip-content]').tooltipster({
+		interactive: true,
+		theme: 'tooltipster-borderless'
+	});
+
+});
+
+/*
+|------------------------------------------------------------------------------
+| Helpers
+|------------------------------------------------------------------------------
+*/
+
+/* nl2br Method Implementation */
+Dom7.nl2br = function(str) {
+	return str.replace(/(\r\n|\n\r|\r|\n)/g, '<br>');
+};
+
+/* br2nl Method Implementation */
+Dom7.br2nl = function(str) {
+	return str.replace(/<br>/g, '\r');
+};
