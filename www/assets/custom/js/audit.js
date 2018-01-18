bb684f1d595d490a01e18b56ffee3d682fda43b8
= <thendo@bbd.co.za>
2018-01-18 13:22:58 +0200

    create account3

---
+++ www/assets/custom/js/audit.js
@@ -0,0 +1,35 @@
+
+// $(document).ready(function(){
+         
+      
+//          	$(document).on('click','#signupBtn',function(){
+//             var username = $('#email').val();
+//             var fname = $('#fname').val();
+//             var sname = $('#sname').val();
+//             var company = $('#company').val();
+//             var contact = $('#contact').val();
+//             var password = $('#password').val();
+           
+//             $.ajax({url: "https://vnpsheq.co.za/Audit?type=save_user&username="+username+"&fname="+fname+"&sname="+sname+"&company="+company+"&contact="+contact+"&password="+password+"", success: function(result){
+	        
+// 	          var data = JSON.parse(result);
+
+// 	          if(data.status == 'Success'){
+// 	          	window.location.href ="home.html?status=Success&uid="+data.user_id+"";
+// 	          }else{
+// 	          window.location.href ="fail.html?";	
+// 	          }
+	          
+// 	        }});
+
+
+//          });
+   
+        
+
+       
+
+
+
+
+// });
--- www/assets/custom/js/components.js
+++ www/assets/custom/js/components.js
@@ -393,6 +393,29 @@ myApp.onPageInit('keypad', function(page) {
 | Modals
 |------------------------------------------------------------------------------
 */
+myApp.onPageInit('signup', function(page) { 
+	$$('#signupBtn').on('click', function(e) {
+		e.preventDefault();
+		    var username = $$('#email').val();
+            var fname = $$('#fname').val();
+            var sname = $$('#sname').val();
+            var company = $$('#company').val();
+            var contact = $$('#contact').val();
+            var password = $$('#password').val();
+            $.ajax({url: "https://vnpsheq.co.za/Audit?type=save_user&username="+username+"&fname="+fname+"&sname="+sname+"&company="+company+"&contact="+contact+"&password="+password+"", success: function(result){
+	        
+	          var data = JSON.parse(result);
+
+	          if(data.status == 'Success'){
+	          	window.location.href ="home.html?status=Success&uid="+data.user_id+"";
+	          }else{
+	          window.location.href ="fail.html?";	
+	          }
+	          
+	        }});            
+		
+	});
+});
 
 myApp.onPageInit('modals', function(page) {
 
